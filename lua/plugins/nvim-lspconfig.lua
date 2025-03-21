0bb9c21d042e46789d8eac7ae961e97d131cbca0
Joan Lucas <joanlucas314@gmail.com>
2025-03-21 19:34:19 -0300

    Removed unecessary space in func definition

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,7 +1,7 @@
 -- https://github.com/neovim/nvim-lspconfig
 return {
     "neovim/nvim-lspconfig",
-    config = function ()
+    config = function()
         require("lsp.pyright").pyright_setup()
         require("lsp.lua_ls").lua_ls_setup()
 	require("lsp.clangd").clangd_setup()
